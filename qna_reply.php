34bb8502bda54ae871191aec88a31ab2566c51f6
happyna97 <119560642+happyna97@users.noreply.github.com>
2023-03-09 12:28:46 +0900

    qna list, qna reply 수정
    qna list 바로가기 qidx연결
    qna reply 일부 데이터 연결

--- qna_reply.php
+++ qna_reply.php
@@ -1,10 +1,13 @@
+<?php 
+    include $_SERVER['DOCUMENT_ROOT']."/teapot/inc/db.php";
+?>
 <!DOCTYPE html>
 <html lang="en">
   <head>
     <meta charset="UTF-8" />
     <meta http-equiv="X-UA-Compatible" content="IE=edge" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <title>TEAPOT | DashBoard</title>
+    <title>TEAPOT | Q&A - Reply</title>
     <script
       src="https://cdn.tiny.cloud/1/gqh4ln9h6t4kj4p3sdrfytsxhn047vp3h815f6ams4i2ou8j/tinymce/5/tinymce.min.js"
       referrerpolicy="origin"
@@ -33,36 +36,47 @@
       rel="stylesheet"
     />
     <link rel="stylesheet" href="../css/common.css" />
-    <link rel="stylesheet" href="../css/admin.css" />
     <link rel="stylesheet" href="../css/qna_reply.css" />
   </head>
   <body>
+    <?php
+      $qidx = $_GET['qidx'];
+      $sql = "SELECT * FROM lms_qna WHERE qidx='".$qidx."'";
+      $result = $mysqli->query($sql) or die("query error => ".$mysqli->error);
+      while($rs = $result->fetch_object()){
+        $rsc[]=$rs;
+      }
+
+      if(isset($rsc)){ 
+        foreach($rsc as $r){
+    ?>
+
     <div id="dashboard">
       <div class="background d-flex flex-column row">
         <div class="qna_reply">
           <h2 class="suit_bold_xl">학습 Q&A</h2>
           <div class="user_question_info">
             <div class="d-flex justify-content-between">
+              <!-- <p class="classname suit_rg_m"><?php echo $r->lec_title;?></p> -->
               <p class="classname suit_rg_m">입문회화</p>
               <p class="reply_status suit_rg_m">답변대기</p>
             </div>
-            <p class="question_title suit_rg_l">전치사 관련 질문입니다.</p>
+            <p class="question_title suit_rg_l"><?php echo $r->qna_title;?></p>
             <div class="reply_info_border">
               <div
                 class="reply_info suit_rg_m d-flex align-items-center justify-content-between"
               >
                 <div class="d-flex align-items-center gap-5">
                   <img src="" alt="" class="userphoto" />
-                  <p class="userid">happyna97</p>
+                  <p class="userid"><?php echo $r->userid;?></p>
                 </div>
-                <p class="date">2023.03.16</p>
+                <p class="date"><?php echo $r->regdate;?></p>
               </div>
             </div>
           </div>
           <div class="user_question">
             <p class="suit_rg_m">
-              "우리는 여름에 초밥 안 먹어." 라고 말할 때 왜 in the summer이고,<br>
-              "우리는 밤에 커피 안 마셔." 라고 할 때는 at afternoon인가요?
+              <?php echo $r->qna_text;?>
             </p>
           </div>
           <div class="admin_aswer_form">
@@ -71,7 +85,7 @@
                 <img src="" alt="" class="adminphoto" />
                 <div class="admin_info">
                   <p class="adminid suit_rg_l">관리자</p>
-                  <p class="date suit_rg_m">2023.03.16</p>
+                  <p class="date suit_rg_m"><?php echo $r->regdate;?></p>
                 </div>
               </div>
               <div class="tiny_library">
@@ -80,7 +94,7 @@
                 </form>
               </div>
               <div class="qna_btn d-flex gap-4 justify-content-end">
-                <button type="submit" class="upload btn_s" onclick="location.href='qna_check.php'">등록</button>
+                <button type="submit" class="upload btn_s" onclick="location.href='qna_.php?qidx=<?php echo $r->qidx;?>'">등록</button>
                 <button type="button" class="cancel btn_s" onclick="location.href='qna_list.php'">취소</button>
               </div>
             </div>
@@ -89,24 +103,10 @@
       </div>
     </div>
 
-    <script
-      src="https://code.jquery.com/jquery-3.6.3.min.js"
-      integrity="sha256-pvPw+upLPUjgMXY0G+8O0xUf+/Im1MZjXxxgOcBQBXU="
-      crossorigin="anonymous"
-    ></script>
-    <script
-      src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js"
-      integrity="sha384-w76AqPfDkMBDXo30jS1Sgez6pr3x5MlQ1ZAGC+nuZB+EYdgRZgiwxhTBTkF7CXvN"
-      crossorigin="anonymous"
-    ></script>
-    <script
-      type="module"
-      src="https://unpkg.com/ionicons@5.5.2/dist/ionicons/ionicons.esm.js"
-    ></script>
-    <script
-      nomodule
-      src="https://unpkg.com/ionicons@5.5.2/dist/ionicons/ionicons.js"
-    ></script>
-    <script src="../js/script.js"></script>
-  </body>
-</html>
+    <?php } } ?>
+  </body> 
+    
+    
+<?php
+include $_SERVER['DOCUMENT_ROOT']."/teapot/inc/admin_footer.php";
+?>
